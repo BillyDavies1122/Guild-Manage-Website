c1e7525b551ecefde0393242b6bb69d454b8f571
BillyDavies1122 <poddiblesc2@gmail.com>
2023-09-30 21:06:30 +0930

    Models , admin page
    Created two models and an admin page super user. See the txt file for login

--- GuildManagerWebsite/Guild/admin.py
+++ GuildManagerWebsite/Guild/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import guild,character
+
 # Register your models here.
+admin.site.register(guild)
+admin.site.register(character)
--- GuildManagerWebsite/Guild/models.py
+++ GuildManagerWebsite/Guild/models.py
@@ -21,8 +21,20 @@ class_choices = (
 class guild(models.Model):
     guildName = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.guildName
+
 
 class character(models.Model):
     characterName = models.CharField(max_length=30)
     characterClass = models.CharField(max_length=50,choices=class_choices)
     characterGuild = models.ForeignKey(guild,on_delete=models.CASCADE)
+
+    def Guildless(self):
+        if self.characterGuild == " ":
+            return "Character is unguilded, would you like to join one?"
+        else:
+            return self.characterGuild
+
+    def __str__(self):
+        return f"Character is called {self.characterName} and is a member of {self.characterGuild}"
